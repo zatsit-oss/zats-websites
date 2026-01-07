9ab5ed09d6f0bb83f4581542d835d662217dd3a8
Ludovic Dussart <ludovic.dussart.pro@gmail.com>
2026-01-07 12:31:40 +0100

    feat(greenscore): add zatsit url on header

--- greenscore-landing/astro.config.mjs
+++ greenscore-landing/astro.config.mjs
@@ -1,7 +1,18 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import relativeLinks from 'astro-relative-links';
+import { defineConfig, envField } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import relativeLinks from "astro-relative-links";
 
 export default defineConfig({
-  integrations: [tailwind(), relativeLinks()]
+  integrations: [tailwind(), relativeLinks()],
+
+  env: {
+    schema: {
+      ZATSIT_WEBSITE_URL: envField.string({
+        required: true,
+        access: "public",
+        context: "client",
+        default: "https://zatsit.fr",
+      }),
+    },
+  },
 });
